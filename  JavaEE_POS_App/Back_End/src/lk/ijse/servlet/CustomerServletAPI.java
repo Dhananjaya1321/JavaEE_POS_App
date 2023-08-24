c7e40c0e02050a922143566fd855f78c99e46086
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-08-24 13:22:21 +0530

    The doPost method of the customerServletAPI has completed

---  JavaEE_POS_App/Back_End/src/lk/ijse/servlet/CustomerServletAPI.java
+++  JavaEE_POS_App/Back_End/src/lk/ijse/servlet/CustomerServletAPI.java
@@ -1,14 +1,24 @@
 package lk.ijse.servlet;
 
+import lk.ijse.db.DBConnection;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/pages/customer")
 public class CustomerServletAPI extends HttpServlet {
+    Connection connection = DBConnection.getDbConnection().getConnection();
+    JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -16,7 +26,39 @@ public class CustomerServletAPI extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Content-Type", "application/json");
+        String nic = req.getParameter("nic");
+        String name = req.getParameter("name");
+        String tel = req.getParameter("tel");
+        String address = req.getParameter("address");
+        System.out.println(nic+name+address+tel);
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO customer VALUES (?,?,?,?)");
+            preparedStatement.setObject(1, nic);
+            preparedStatement.setObject(2, name);
+            preparedStatement.setObject(3, tel);
+            preparedStatement.setObject(4, address);
+            if (preparedStatement.executeUpdate() > 0) {
+                resp.getWriter().print(
+                        objectBuilder
+                                .add("state", "Ok")
+                                .add("message", "Successfully Added...!")
+                                .add("data", "[]")
+                                .build()
+                );
+            }
 
+        } catch (SQLException e) {
+            resp.addHeader("Content-Type", "application/json");
+            resp.setStatus(400);
+            resp.getWriter().print(
+                    objectBuilder
+                            .add("state", "Error")
+                            .add("message", e.getMessage())
+                            .add("data", "[]")
+                            .build()
+            );        }
     }
 
     @Override
