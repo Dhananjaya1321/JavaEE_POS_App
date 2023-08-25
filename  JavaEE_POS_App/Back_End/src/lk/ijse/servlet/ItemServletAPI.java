f16a8510ef968a39d6275c5f805f9fbe77cb31c4
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-08-25 11:23:47 +0530

    The doGet method of the itemServletAPI has completed

---  JavaEE_POS_App/Back_End/src/lk/ijse/servlet/ItemServletAPI.java
+++  JavaEE_POS_App/Back_End/src/lk/ijse/servlet/ItemServletAPI.java
@@ -1,17 +1,45 @@
 package lk.ijse.servlet;
 
+import lk.ijse.db.DBConnection;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/pages/item")
 public class ItemServletAPI extends HttpServlet {
+    Connection connection = DBConnection.getDbConnection().getConnection();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin","*");
+        resp.addHeader("Content-Type","application/json");
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM item");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            while (resultSet.next()){
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("code",resultSet.getString(1));
+                objectBuilder.add("name",resultSet.getString(2));
+                objectBuilder.add("price",resultSet.getString(3));
+                objectBuilder.add("qty",resultSet.getString(4));
+                arrayBuilder.add(objectBuilder.build());
+            }
+            resp.getWriter().print(arrayBuilder.build());
+        } catch (SQLException e) {
 
+        }
     }
 
     @Override
