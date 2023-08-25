59383590d19428a81d8d49492eac0f3c4b8465c3
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-08-25 22:41:00 +0530

    Created searchCustomer function and get selected valuse using combobox

---  JavaEE_POS_App/Front_End/controllers/orderFormController.js
+++  JavaEE_POS_App/Front_End/controllers/orderFormController.js
@@ -3,6 +3,9 @@ $(window).ready(function () {
     getAllCustomersForComboBox();
 })
 
+let customers;
+let items;
+
 function loadAllItemsForComboBox() {
     $.ajax({
         url: "http://localhost:8080/pos_app/pages/item",
@@ -12,6 +15,9 @@ function loadAllItemsForComboBox() {
             $("#item-itemCode").append(
                 `<option>Select Code</option>`
             );
+
+            items=resp;
+
             for (let i in resp) {
                 let item = resp[i];
                 let code = item.code;
@@ -30,13 +36,16 @@ function getAllCustomersForComboBox() {
     $.ajax({
         url: "http://localhost:8080/pos_app/pages/customer",
         method: "get",
-        success: function (rep) {
+        success: function (resp) {
             $("#invoice-customerNIC").empty();
             $("#invoice-customerNIC").append(
                 `<option>Select NIC</option>`
             );
-            for (let i in rep) {
-                let customer = rep[i];
+
+            customers=resp;
+
+            for (let i in resp) {
+                let customer = resp[i];
                 let nic = customer.nic;
                 $("#invoice-customerNIC").append(
                     `<option>${nic}</option>`
@@ -44,4 +53,30 @@ function getAllCustomersForComboBox() {
             }
         }
     });
+}
+
+/*invoice*/
+$("#invoice-customerNIC").click(function () {
+    let nic = $("#invoice-customerNIC").val();
+    if (nic !== "Select NIC") {
+        let customer = searchCustomer(nic);
+        $("#customerName").val(customer.name);
+        $("#customerTel").val(customer.tel);
+        $("#customerAddress").val(customer.address);
+
+        $("#invoice-customerNIC").css("border", 'solid green 2px');
+        $("#customerName").css("border", 'solid green 2px');
+        $("#customerTel").css("border", 'solid green 2px');
+        $("#customerAddress").css("border", 'solid green 2px');
+    } else {
+        $("#customerName").val("");
+        $("#customerTel").val("");
+        $("#customerAddress").val("");
+    }
+});
+
+function searchCustomer(nic) {
+    return customers.find(function (customer) {
+        return customer.nic === nic;
+    });//return to matched customer object
 }
