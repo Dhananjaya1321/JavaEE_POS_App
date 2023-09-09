715b2079dec319f54cf088646292571fe185aa3c
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-09-09 14:46:55 +0530

    Created CrudUtil class and setQuery method to that class

---
+++  JavaEE_POS_App/Back_End/src/lk/ijse/util/CrudUtil.java
@@ -0,0 +1,11 @@
+package lk.ijse.util;
+
+public class CrudUtil {
+    public static <T> T setQuery(String query, Object... args) {
+        if (query.startsWith("SELECT") || query.startsWith("select")) {
+            return null;
+        } else {
+            return null;
+        }
+    }
+}
