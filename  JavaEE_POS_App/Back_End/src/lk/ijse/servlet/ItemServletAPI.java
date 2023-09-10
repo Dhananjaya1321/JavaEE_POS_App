cc6102b03b7ce5875d8090790ed5dc8b6c157092
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-09-10 14:09:23 +0530

    Created ResponseUtil and ItemServletAPI was updated

---  JavaEE_POS_App/Back_End/src/lk/ijse/servlet/ItemServletAPI.java
+++  JavaEE_POS_App/Back_End/src/lk/ijse/servlet/ItemServletAPI.java
@@ -5,6 +5,7 @@ import lk.ijse.bo.BOTypes;
 import lk.ijse.bo.FactoryBO;
 import lk.ijse.bo.castom.impl.ItemBOImpl;
 import lk.ijse.dto.ItemDTO;
+import lk.ijse.util.ResponseUtil;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.json.*;
@@ -26,8 +27,6 @@ public class ItemServletAPI extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-
         ServletContext servletContext = getServletContext();
         BasicDataSource dbcp = (BasicDataSource) servletContext.getAttribute("dbcp");
 
@@ -45,121 +44,54 @@ public class ItemServletAPI extends HttpServlet {
                         .build()
                 );
             }
-            resp.getWriter().print(
-                    objectBuilder
-                            .add("state", "Ok")
-                            .add("message", "Successfully loaded...!")
-                            .add("data", "[" + arrayBuilder.build() + "]")
-                            .build()
-            );
+
+            resp.getWriter().print(ResponseUtil.getResJsonObject("Ok","Successfully loaded...!",arrayBuilder.build()));
         } catch (SQLException e) {
             resp.setStatus(400);
-            resp.getWriter().print(
-                    objectBuilder
-                            .add("state", "Error")
-                            .add("message", e.getMessage())
-                            .add("data", "[]")
-                            .build()
-            );
+            resp.getWriter().print(ResponseUtil.getResJsonObject("Error","Not loaded...!"));
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-
-        String code = req.getParameter("code");
-        String name = req.getParameter("name");
-        double price = Double.parseDouble(req.getParameter("price"));
-        int qty = Integer.parseInt(req.getParameter("qty"));
-
         try {
-            if (itemBO.addItem(new ItemDTO(code, name, price, qty))) {
-                resp.getWriter().print(
-                        objectBuilder
-                                .add("state", "Ok")
-                                .add("message", "Successfully Added...!")
-                                .add("data", "[]")
-                                .build()
-                );
+            if (itemBO.addItem(new ItemDTO(req.getParameter("code"), req.getParameter("name"), Double.parseDouble(req.getParameter("price")), Integer.parseInt(req.getParameter("qty"))))) {
+                resp.getWriter().print(ResponseUtil.getResJsonObject("Error","Successfully Added...!"));
             }else {
-                resp.getWriter().print(
-                        objectBuilder
-                                .add("state", "Error")
-                                .add("message", "Not Added...!")
-                                .add("data", "[]")
-                                .build()
-                );
+                resp.getWriter().print(ResponseUtil.getResJsonObject("Error","Not Added...!"));
             }
         } catch (SQLException e) {
             resp.setStatus(400);
-            resp.getWriter().print(
-                    objectBuilder
-                            .add("state", "Error")
-                            .add("message", e.getMessage())
-                            .add("data", "[]")
-                            .build()
-            );
+            resp.getWriter().print(ResponseUtil.getResJsonObject("Error",e.getMessage()));
         }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
         JsonObject jsonObject = Json.createReader(req.getReader()).readObject();
-
         try  {
             if (itemBO.updateItem(new ItemDTO(jsonObject.getString("code"),jsonObject.getString("name"),Double.parseDouble(jsonObject.getString("price")),Integer.parseInt(jsonObject.getString("qty"))))) {
-                resp.getWriter().print(
-                        objectBuilder
-                                .add("state", "Ok")
-                                .add("message", "Successfully Updated...!")
-                                .add("data", "[]")
-                                .build()
-                );
+                resp.getWriter().print(ResponseUtil.getResJsonObject("OK","Successfully Updated...!"));
             }else {
-                resp.getWriter().print(
-                        objectBuilder
-                                .add("state", "Error")
-                                .add("message", "Not Updated...!")
-                                .add("data", "[]")
-                                .build()
-                );
+                resp.getWriter().print(ResponseUtil.getResJsonObject("Error","Not Updated...!"));
             }
         } catch (SQLException e) {
             resp.setStatus(400);
-            resp.getWriter().print(
-                    objectBuilder
-                            .add("state", "Error")
-                            .add("message", e.getMessage())
-                            .add("data", "[]")
-                            .build()
-            );
+            resp.getWriter().print(ResponseUtil.getResJsonObject("Error",e.getMessage()));
         }
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
         try  {
             if (itemBO.deleteItem(new ItemDTO(req.getParameter("code")))) {
-                resp.getWriter().print(
-                        objectBuilder
-                                .add("state", "Ok")
-                                .add("message", "Successfully Deleted...!")
-                                .add("data", "[]")
-                                .build()
-                );
+                resp.getWriter().print(ResponseUtil.getResJsonObject("Ok","Successfully Deleted...!"));
+            }else {
+                resp.getWriter().print(ResponseUtil.getResJsonObject("Error","Not Deleted...!"));
             }
         } catch (SQLException e) {
             resp.setStatus(400);
-            resp.getWriter().print(
-                    objectBuilder
-                            .add("state", "Error")
-                            .add("message", e.getMessage())
-                            .add("data", "[]")
-                            .build()
-            );
+            resp.getWriter().print(ResponseUtil.getResJsonObject("Error",e.getMessage()));
         }
     }
 }
