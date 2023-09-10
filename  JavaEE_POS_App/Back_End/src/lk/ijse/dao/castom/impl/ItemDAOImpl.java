0a255e84ee0401ce174700d98009bfe57b9f952f
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-09-10 13:10:14 +0530

    Created add,update,delete methods of ItemDAOImpl

---  JavaEE_POS_App/Back_End/src/lk/ijse/dao/castom/impl/ItemDAOImpl.java
+++  JavaEE_POS_App/Back_End/src/lk/ijse/dao/castom/impl/ItemDAOImpl.java
@@ -2,6 +2,7 @@ package lk.ijse.dao.castom.impl;
 
 import lk.ijse.dao.castom.ItemDAO;
 import lk.ijse.entity.Item;
+import lk.ijse.util.CrudUtil;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -9,17 +10,17 @@ import java.util.ArrayList;
 public class ItemDAOImpl implements ItemDAO {
     @Override
     public boolean add(Item to) throws SQLException {
-        return false;
+        return CrudUtil.setQuery("INSERT INTO item VALUES (?,?,?,?)", to.getCode(), to.getName(), to.getPrice(), to.getQty());
     }
 
     @Override
     public boolean update(Item to) throws SQLException {
-        return false;
+        return CrudUtil.setQuery("UPDATE item SET name=?,price=?,qty=? WHERE code=?", to.getName(), to.getPrice(), to.getQty(), to.getCode());
     }
 
     @Override
     public boolean delete(Item to) throws SQLException {
-        return false;
+        return CrudUtil.setQuery("DELETE FROM item WHERE code=? ", to.getCode());
     }
 
     @Override
